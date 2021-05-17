cedc978e215a90d83cad58ae311a077f3e5fbe4c
Jaewon <sorhd134@gmail.com>
2021-05-17 16:17:59 +0900

    Build home tab navigator structure

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text } from "react-native";
+import styled from "styled-components/native";
+
+const Container = styled.View``;
+
+const Home = () => {
+  return (
+    <Container>
+      <Text>Home</Text>
+    </Container>
+  );
+};
+
+export default Home;
---
+++ src/screens/Location/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text } from "react-native";
+import styled from "styled-components/native";
+
+const Container = styled.View``;
+
+const Location = () => {
+  return (
+    <Container>
+      <Text>Location</Text>
+    </Container>
+  );
+};
+
+export default Location;
