f97786de53b70fdf85c450212a69ab985ad0e76e
Jaewon <sorhd134@gmail.com>
2021-05-20 14:20:16 +0900

    Bluetooth test

--- src/screens/Location/index.tsx
+++ src/screens/Location/index.tsx
@@ -1,9 +1,20 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useEffect } from "react";
+import {
+  NativeEventEmitter,
+  NativeModules,
+  Text,
+  TouchableOpacity,
+} from "react-native";
 import styled from "styled-components/native";
 import { useAppSelector } from "~/store";
 import { LocationScreenNavigationProp } from "~/types/navigator";
 import AuthSelector from "../Shared/AuthSelector";
+
+import BleManager from "react-native-ble-manager";
+import AndroidOpenSettings from "react-native-android-open-settings";
+
+const BleManagerModule = NativeModules.BleManager;
+const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
 
 const Container = styled.View``;
 
@@ -14,11 +25,39 @@ const Location = ({
 }) => {
   const { isLoggedIn } = useAppSelector(state => state.user);
 
+  const handleOpenSetting = () => {
+    AndroidOpenSettings.bluetoothSettings();
+  };
+
+  const getPeripheral = (peripheral: any) => {
+    console.log(peripheral);
+  };
+
+  useEffect(() => {
+    BleManager.start({ showAlert: false }).then(() => {
+      console.log("Initialized");
+      // 최초 페어링을 할 시 기기의 시리얼 넘버를 받아오기 위함
+      bleManagerEmitter.addListener(
+        "BleManagerConnectPeripheral",
+        getPeripheral,
+      );
+    });
+
+    return () =>
+      bleManagerEmitter.removeListener(
+        "BleManagerConnectPeripheral",
+        getPeripheral,
+      );
+  }, []);
+
   if (!isLoggedIn) return <AuthSelector />;
 
   return (
     <Container>
       <Text>Location</Text>
+      <TouchableOpacity onPress={handleOpenSetting}>
+        <Text style={{ fontSize: 40 }}>Open bluetooth setting</Text>
+      </TouchableOpacity>
     </Container>
   );
 };
