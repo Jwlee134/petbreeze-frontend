fe06a65fcda08ef22c4ea2c599b13ebe29e5f791
Jaewon <sorhd134@gmail.com>
2021-06-04 11:05:37 +0900

    Warning fix: RCTBridge required dispatch_sync to load RCTDevLoadingView.

--- ios/Petbreeze/AppDelegate.m
+++ ios/Petbreeze/AppDelegate.m
@@ -1,5 +1,9 @@
 #import "AppDelegate.h"
 
+#if RCT_DEV
+#import <React/RCTDevLoadingView.h>
+#endif
+
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -32,7 +36,14 @@ static void InitializeFlipper(UIApplication *application) {
   InitializeFlipper(application);
 #endif
 
+  NSURL *jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.js" fallbackResource:nil];
+
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+
+  #if RCT_DEV
+   [bridge moduleForClass:[RCTDevLoadingView class]];
+  #endif
+
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"Petbreeze"
                                             initialProperties:nil];
