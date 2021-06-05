17a77f377abec4c87a7b5adc249a1da71f1480eb
Jaewon <sorhd134@gmail.com>
2021-06-05 19:45:52 +0900

    Create comment component

--- src/screens/Shared/Comment.tsx
+++ src/screens/Shared/Comment.tsx
@@ -1,17 +1,52 @@
+import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
 import React from "react";
-import { KeyboardAvoidingView, ScrollView } from "react-native";
+import { Platform, ScrollView } from "react-native";
+import styled from "styled-components/native";
 import CategoryTitle from "~/components/common/CategoryTitle";
-import Input from "~/components/common/input/Input";
+import Input from "~/components/common/Input";
+import palette from "~/styles/palette";
 
-const Comment = () => (
-  <>
-    <ScrollView>
-      <CategoryTitle>댓글</CategoryTitle>
-    </ScrollView>
-    <KeyboardAvoidingView>
-      <Input />
-    </KeyboardAvoidingView>
-  </>
-);
+const InputContainer = styled.KeyboardAvoidingView``;
+
+const SubmitButton = styled.TouchableOpacity`
+  background-color: ${palette.blue_6e};
+  width: 44px;
+  height: 28px;
+  border-radius: 4px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Text = styled.Text`
+  color: white;
+`;
+
+const Comment = () => {
+  const height = useBottomTabBarHeight();
+
+  return (
+    <>
+      <ScrollView>
+        <CategoryTitle>댓글</CategoryTitle>
+      </ScrollView>
+      <InputContainer
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+        keyboardVerticalOffset={
+          Platform.OS === "ios" ? height + 11 : undefined
+        }>
+        <Input
+          style={{ paddingRight: 66, borderRadius: 0 }}
+          hasShadow={false}
+          isMultiline
+          RightIcon={() => (
+            <SubmitButton onPress={() => {}} activeOpacity={0.8}>
+              <Text>입력</Text>
+            </SubmitButton>
+          )}
+        />
+      </InputContainer>
+    </>
+  );
+};
 
 export default Comment;
