587da8be348ec1c302ef932b82e62b92dcc25cdb
Jaewon <sorhd134@gmail.com>
2021-08-18 15:12:53 +0900

    Update bottom tab style

--- src/navigator/BottomTabNav.tsx
+++ src/navigator/BottomTabNav.tsx
@@ -14,54 +14,82 @@ import UserOutline from "~/assets/svg/tab/user-outline.svg";
 import CustomHeader from "~/components/navigator/CustomHeader";
 import { BottomTabNavRouteProp } from "~/types/navigator";
 import WalkStackNav from "./WalkStackNav";
+import { rpWidth } from "~/styles";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const Tab = createBottomTabNavigator();
 
-const BottomTabNav = ({ route }: { route: BottomTabNavRouteProp }) => (
-  <Tab.Navigator
-    initialRouteName={
-      route?.params?.initialTab?.includes("Walk") ? "WalkStackNav" : "HomeTab"
-    }
-    screenOptions={{
-      tabBarShowLabel: false,
-      tabBarActiveTintColor: palette.blue_7b,
-      tabBarInactiveTintColor: "rgba(0, 0, 0, 0.3)",
-    }}>
-    <Tab.Screen
-      name="HomeTab"
-      options={{
-        headerShown: false,
-        tabBarIcon: ({ focused }) => (focused ? <Home /> : <HomeOutline />),
+const BottomTabNav = ({ route }: { route: BottomTabNavRouteProp }) => {
+  const { bottom } = useSafeAreaInsets();
+
+  return (
+    <Tab.Navigator
+      initialRouteName={
+        route?.params?.initialTab?.includes("Walk") ? "WalkStackNav" : "HomeTab"
+      }
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: palette.blue_7b,
+        tabBarInactiveTintColor: "rgba(0, 0, 0, 0.3)",
+        tabBarStyle: {
+          height: rpWidth(52) + bottom,
+        },
       }}>
-      {() => <SharedStackNav screenName="Home" />}
-    </Tab.Screen>
-    <Tab.Screen
-      name="WalkStackNav"
-      component={WalkStackNav}
-      initialParams={{
-        initialTab: route?.params?.initialTab,
-      }}
-      options={{
-        header: () => <CustomHeader>산책</CustomHeader>,
-        tabBarIcon: ({ focused }) =>
-          focused ? <Footprint /> : <FootprintOutline />,
-      }}
-    />
-    <Tab.Screen
-      name="NotificationTab"
-      options={{
-        tabBarIcon: ({ focused }) => (focused ? <Bell /> : <BellOutline />),
-      }}>
-      {() => <SharedStackNav screenName="Notification" />}
-    </Tab.Screen>
-    <Tab.Screen
-      name="MyPageTab"
-      options={{
-        tabBarIcon: ({ focused }) => (focused ? <User /> : <UserOutline />),
-      }}>
-      {() => <SharedStackNav screenName="MyPage" />}
-    </Tab.Screen>
-  </Tab.Navigator>
-);
+      <Tab.Screen
+        name="HomeTab"
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <Home width={rpWidth(24)} height={rpWidth(23)} />
+            ) : (
+              <HomeOutline width={rpWidth(24)} height={rpWidth(23)} />
+            ),
+        }}>
+        {() => <SharedStackNav screenName="Home" />}
+      </Tab.Screen>
+      <Tab.Screen
+        name="WalkStackNav"
+        component={WalkStackNav}
+        initialParams={{
+          initialTab: route?.params?.initialTab,
+        }}
+        options={{
+          header: () => <CustomHeader>산책</CustomHeader>,
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <Footprint width={rpWidth(25)} height={rpWidth(24)} />
+            ) : (
+              <FootprintOutline width={rpWidth(25)} height={rpWidth(24)} />
+            ),
+        }}
+      />
+      <Tab.Screen
+        name="NotificationTab"
+        options={{
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <Bell width={rpWidth(21)} height={rpWidth(24)} />
+            ) : (
+              <BellOutline width={rpWidth(21)} height={rpWidth(24)} />
+            ),
+        }}>
+        {() => <SharedStackNav screenName="Notification" />}
+      </Tab.Screen>
+      <Tab.Screen
+        name="MyPageTab"
+        options={{
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <User width={rpWidth(22)} height={rpWidth(23)} />
+            ) : (
+              <UserOutline width={rpWidth(22)} height={rpWidth(23)} />
+            ),
+        }}>
+        {() => <SharedStackNav screenName="MyPage" />}
+      </Tab.Screen>
+    </Tab.Navigator>
+  );
+};
 
 export default BottomTabNav;
