19ff6db1dc303c7d4e70dc9a28120626503adb2e
Jaewon <sorhd134@gmail.com>
2021-10-05 10:34:50 +0900

    Update notification screen

--- src/screens/sharedStackNav/Notification.tsx
+++ src/screens/sharedStackNav/Notification.tsx
@@ -1,24 +1,61 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NotificationScreenNavigationProp } from "~/types/navigator";
-import useModal from "~/hooks/useModal";
+import styled, { css } from "styled-components/native";
 
-import Modal from "react-native-modal";
-import NotificationModal from "~/components/modal/NotificationModal";
-import { useLayoutEffect } from "react";
-import HeaderRightButton from "~/components/common/button/HeaderRightButton";
+import notifications from "~/assets/notification.json";
+import Empty from "~/components/notification/Empty";
+import MyText from "~/components/common/MyText";
+import { DimensionsContext, RpWidth } from "~/context/DimensionsContext";
+import { Animated } from "react-native";
+import Divider from "~/components/common/Divider";
+import NotificationItem from "~/components/notification/NotificationItem";
+
+const Container = styled.ScrollView`
+  flex: 1;
+`;
+
+const CategoryTitle = styled.View<{ rpWidth: RpWidth }>`
+  ${({ rpWidth }) => css`
+    margin-top: ${rpWidth(25)}px;
+    margin-left: ${rpWidth(41)}px;
+    margin-bottom: ${rpWidth(30)}px;
+  `}
+`;
 
 const Notification = ({
   navigation,
 }: {
   navigation: NotificationScreenNavigationProp;
 }) => {
-  /*   useLayoutEffect(() => {
-    navigation.setOptions({
-      headerRight: () => <HeaderRightButton open={open} />,
-    });
-  }, []); */
+  const { rpWidth } = useContext(DimensionsContext);
 
-  return <></>;
+  return notifications.length ? (
+    <Container>
+      <CategoryTitle rpWidth={rpWidth}>
+        <MyText fontWeight="medium" fontSize={18}>
+          새로운 알림
+        </MyText>
+      </CategoryTitle>
+      {notifications
+        .filter(notif => !notif.read)
+        .map((notif, i) => (
+          <NotificationItem key={i} data={notif} />
+        ))}
+      <Divider />
+      <CategoryTitle rpWidth={rpWidth}>
+        <MyText fontWeight="medium" fontSize={18}>
+          이번주
+        </MyText>
+      </CategoryTitle>
+      {notifications
+        .filter(notif => notif.read)
+        .map((notif, i) => (
+          <NotificationItem key={i} data={notif} />
+        ))}
+    </Container>
+  ) : (
+    <Empty />
+  );
 };
 
 export default Notification;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,7 +5,6 @@ import {
   PERMISSIONS,
   request,
 } from "react-native-permissions";
-import { IReverseGeocoding } from "~/types/geocoding";
 
 export const isAndroid = Platform.OS === "android";
 
@@ -14,65 +13,8 @@ export const isIos = Platform.OS === "ios";
 export const bytesToString = (bytes: number[]) =>
   bytes.map(x => String.fromCharCode(x)).join("");
 
-export const insertPointToString = (text: string) =>
-  text.replace(/(\d{4})(\d{2})(\d{2})/, "$1. $2. $3");
-
-export const insertDashToString = (text: string) =>
-  text.replace(/(\d{3})(\d{4})(\d{4})/, "$1-$2-$3");
-
-export const formatUTC = (dataArray: number[]) => {
-  const date = (dataArray[0] << 12) | (dataArray[1] << 4) | (dataArray[2] >> 4);
-
-  const utc =
-    ((dataArray[2] & 0x0f) << 16) | (dataArray[3] << 8) | dataArray[4];
-
-  return { date, utc };
-};
-
-export const formatCoordinates = (dataArray: number[]) => {
-  let _lat =
-    (((dataArray[0] & 0x7f) << 20) |
-      (dataArray[1] << 12) |
-      (dataArray[2] << 4) |
-      (dataArray[3] >> 4)) /
-    1000000;
-  const lat = _lat * (dataArray[0] >> 7 ? -1 : 1);
-
-  let _lng =
-    (((dataArray[3] & 0x0f) << 24) |
-      (dataArray[4] << 16) |
-      (dataArray[5] << 8) |
-      dataArray[6]) /
-    1000000;
-  const lng = _lng * ((dataArray[3] & 0x0f) >> 3 ? -1 : 1);
-
-  return { lat, lng };
-};
-
-export const formatGeocodingAddr = (data: IReverseGeocoding) => {
-  return data.results.map(({ region, land }) => {
-    let fullAddr = "";
-
-    const regionName = Object.values(region)
-      .filter((area, index) => {
-        if (index === 0) return false;
-        return area.name;
-      })
-      .map(area => area.name)
-      .join(" ");
-
-    if (land) {
-      if (land.number1 && !land.number2) {
-        fullAddr = `${regionName} ${land.number1}`;
-      } else if (land.number1 && land.number2) {
-        fullAddr = `${regionName} ${land.number1}-${land.number2}`;
-      }
-    } else {
-      fullAddr = regionName;
-    }
-    return fullAddr;
-  });
-};
+export const stringToBytes = (str: string): number[] =>
+  str.split("").map(x => x.charCodeAt(0));
 
 export const getLeftRightPointsOfCircle = (
   latitude: number,
@@ -110,13 +52,10 @@ export const formatCreatedAt = (createdAt: string) => {
   const hours = minutes / 60;
   if (hours < 24) return `${Math.floor(hours)}시간 전`;
   const days = hours / 24;
-  if (days < 7) return `${Math.floor(days)}일 전`;
+  if (days < 7)
+    return Math.floor(days) === 1 ? "어제" : `${Math.floor(days)}일 전`;
   const weeks = days / 7;
   if (weeks < 5) return `${Math.floor(weeks)}주 전`;
-  const months = days / 30;
-  if (months < 12) return `${Math.floor(months)}개월 전`;
-  const years = days / 365;
-  return `${Math.floor(years)}년 전`;
 };
 
 export const getDistanceBetween2Points = (
