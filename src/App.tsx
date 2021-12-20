8117ece35208026905ccb5c1ce924e406d8b44a8
Jaewon <sorhd134@gmail.com>
2021-12-20 16:34:55 +0900

    Update: toast topOffset

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,25 @@
-import React, { useMemo } from "react";
-import {
-  NavigationContainer,
-  useNavigationContainerRef,
-} from "@react-navigation/native";
+import React from "react";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 import { Settings } from "react-native-fbsdk-next";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import theme from "./styles/theme";
 import { persister, store } from "./store";
 import RootNav from "./navigator/RootNav";
 import DimensionsContextProvider from "./context/DimensionsContext";
-import { useFlipper } from "@react-navigation/devtools";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-
-import Toast, { BaseToast, BaseToastProps } from "react-native-toast-message";
-import toastStyle, { ToastType } from "./styles/toast";
 
 Settings.initializeSDK();
 
 const App = () => {
-  const navigationRef = useNavigationContainerRef();
-  const toastConfig = useMemo(
-    () => ({
-      notification: ({ ...rest }: BaseToastProps) => (
-        <BaseToast {...rest} {...toastStyle(ToastType.Notification)} />
-      ),
-      error: ({ ...rest }: BaseToastProps) => (
-        <BaseToast {...rest} {...toastStyle(ToastType.Error)} />
-      ),
-    }),
-    [],
-  );
-
-  useFlipper(navigationRef);
-
   return (
     <Provider store={store}>
       <PersistGate persistor={persister}>
         <DimensionsContextProvider>
-          <NavigationContainer ref={navigationRef} theme={theme}>
-            <SafeAreaProvider>
-              <GestureHandlerRootView style={{ flex: 1 }}>
-                <RootNav />
-              </GestureHandlerRootView>
-            </SafeAreaProvider>
-          </NavigationContainer>
-          <Toast config={toastConfig} />
+          <SafeAreaProvider>
+            <GestureHandlerRootView style={{ flex: 1 }}>
+              <RootNav />
+            </GestureHandlerRootView>
+          </SafeAreaProvider>
         </DimensionsContextProvider>
       </PersistGate>
     </Provider>
--- src/navigator/RootNav.tsx
+++ src/navigator/RootNav.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { useAppSelector } from "~/store";
 import LoggedInNav from "./LoggedInNav";
 import { StatusBar } from "react-native";
@@ -8,12 +8,37 @@ import { RootNavParamList } from "~/types/navigator";
 import Intro from "~/screens/rootNav/Intro";
 import Auth from "~/screens/rootNav/Auth";
 import { createStackNavigator } from "@react-navigation/stack";
+import {
+  NavigationContainer,
+  useNavigationContainerRef,
+} from "@react-navigation/native";
+import theme from "~/styles/theme";
+import { useFlipper } from "@react-navigation/devtools";
+import Toast, { BaseToast, BaseToastProps } from "react-native-toast-message";
+import toastStyle, { ToastType } from "~/styles/toast";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const Stack = createStackNavigator<RootNavParamList>();
 
 const RootNav = () => {
+  const navigationRef = useNavigationContainerRef();
   const { isCodePushUpdated, isIntroPassed } = useAppSelector(
     state => state.storage.init,
+  );
+  const { top } = useSafeAreaInsets();
+
+  useFlipper(navigationRef);
+
+  const toastConfig = useMemo(
+    () => ({
+      notification: ({ ...rest }: BaseToastProps) => (
+        <BaseToast {...rest} {...toastStyle(ToastType.Notification)} />
+      ),
+      error: ({ ...rest }: BaseToastProps) => (
+        <BaseToast {...rest} {...toastStyle(ToastType.Error)} />
+      ),
+    }),
+    [],
   );
 
   return (
@@ -23,28 +48,31 @@ const RootNav = () => {
         barStyle="dark-content"
         backgroundColor="transparent"
       />
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-          cardStyleInterpolator: ({ current }) => ({
-            cardStyle: { opacity: current.progress },
-          }),
-        }}
-        initialRouteName={(() => {
-          if (!isCodePushUpdated) {
-            return "FirmwareUpdate";
-          }
-          if (!isIntroPassed) {
-            return "Intro";
-          }
-          return "Start";
-        })()}>
-        <Stack.Screen name="FirmwareUpdate" component={FirmwareUpdate} />
-        <Stack.Screen name="Intro" component={Intro} />
-        <Stack.Screen name="Auth" component={Auth} />
-        <Stack.Screen name="Start" component={Start} />
-        <Stack.Screen name="LoggedInNav" component={LoggedInNav} />
-      </Stack.Navigator>
+      <NavigationContainer ref={navigationRef} theme={theme}>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+            cardStyleInterpolator: ({ current }) => ({
+              cardStyle: { opacity: current.progress },
+            }),
+          }}
+          initialRouteName={(() => {
+            if (!isCodePushUpdated) {
+              return "FirmwareUpdate";
+            }
+            if (!isIntroPassed) {
+              return "Intro";
+            }
+            return "Start";
+          })()}>
+          <Stack.Screen name="FirmwareUpdate" component={FirmwareUpdate} />
+          <Stack.Screen name="Intro" component={Intro} />
+          <Stack.Screen name="Auth" component={Auth} />
+          <Stack.Screen name="Start" component={Start} />
+          <Stack.Screen name="LoggedInNav" component={LoggedInNav} />
+        </Stack.Navigator>
+      </NavigationContainer>
+      <Toast config={toastConfig} topOffset={top + 11} />
     </>
   );
 };
